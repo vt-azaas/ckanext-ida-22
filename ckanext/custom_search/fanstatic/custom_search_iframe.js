5d7b3be726a65ccf54c8cfc373dc8448b71c33db
VT Azaas <v-vt@azaas.com>
2014-10-20 10:20:23 +0000

    make tags searchable

--- ckanext/custom_search/fanstatic/custom_search_iframe.js
+++ ckanext/custom_search/fanstatic/custom_search_iframe.js
@@ -12,7 +12,14 @@ function getParameterByName(name) {
 ckan.module('custom_search_iframe', function ($, _) {
    return {
      initialize: function() {
-       this.el.html("<iframe frameborder='0' src='http://daas-dap.cloudapp.net/search?q=" + getParameterByName("q") + "' style='height:1800px;width:100%' />");
+       var q = getParameterByName("q");
+       if (q == "")
+       {
+          var q = getParameterByName("tags");
+       }
+
+       this.el.html("<iframe frameborder='0' src='http://daas-dap.cloudapp.net/search?q=" + q + "' style='height:1800px;width:100%' />");
+       //console.log( getParameterByName('q'));
      }
    };
 });
